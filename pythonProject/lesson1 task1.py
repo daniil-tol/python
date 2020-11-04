7d992b9668b469a558cbd20571d573e6295681b5
daniil-tol <daniil.tolk@gamil.com>
2020-11-04 17:06:30 +0300

    task1

---
+++ pythonProject/lesson1 task1.py
@@ -0,0 +1,8 @@
+name = input("What yor Name? ")
+print("Hi!" + name)
+country = input("What country do you live in? ")
+old = input("How ald are you? ")
+number = input("What's your favorite number? ")
+
+print(f"Your name {name}. Your are {old} years old. You live in {country}.Your favourite number {number}.")
+
